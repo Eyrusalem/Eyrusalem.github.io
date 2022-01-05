b3c58e3a6c93d16120dd0b156a453145a69b3ced
Eyrusalem <ruth17.et@gmail.com>
2022-01-06 00:10:12 +0300

    editted

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-// On Click hand emoji will take you to the top of the page
+
 document.getElementById('top-button').addEventListener('click',function(){
     window.scrollTo(0,0);
 });
@@ -23,12 +23,12 @@ $('#switch1').on('click', function(){
 })
 
 
-// If user visit after 7 pm in night the body will change the style to night class
+
 $(document).ready(function(){
   var date = new Date();
   var current_time = date.getHours();
   if (current_time > 19 || current_time < 6)
-    // If time is after 7PM or before 6AM, apply night theme to ‘body’
+
     document.body.className = "night";
   else
     // Else use ‘day’ theme
@@ -40,7 +40,7 @@ $(window).load(function() {
 });
 
 
-// If you hover over the languages I used in project Box it will do fade in animation
+
 $('.project_used span').on({
     mouseover: function() {
         event.preventDefault();
